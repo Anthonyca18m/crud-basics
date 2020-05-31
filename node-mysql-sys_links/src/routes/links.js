4ea1414bf9c153570141e94251fc9fb844a68438
anthonyca18m <anthonyca18m@gmail.com>
2020-05-30 22:04:00 -0500

    Nodejs 8 => Listando los links guardados desde la db y danto formato a fechas con timeago.js

--- node-mysql-sys_links/src/routes/links.js
+++ node-mysql-sys_links/src/routes/links.js
@@ -17,7 +17,12 @@ router.post('/add', async (req, res) => {
 
     await pool.query('INSERT INTO links SET ?', [newLink]);
 
-    res.send('received');
+    res.redirect('/links');
+});
+
+router.get('/', async (req, res) => {
+    const links = await pool.query('SELECT * FROM links');
+    res.render('links/list', {links});
 });
 
 module.exports = router;
