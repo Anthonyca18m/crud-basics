9b7d7640e62936ca91e19dc0d4241ee9f0d1ef02
anthonyca18m <anthonyca18m@gmail.com>
2020-05-30 23:06:14 -0500

    Nodejs 10 => Editando una nota de link desde la db

--- node-mysql-sys_links/src/routes/links.js
+++ node-mysql-sys_links/src/routes/links.js
@@ -31,4 +31,21 @@ router.get('/delete/:id', async (req, res) => {
     res.redirect('/links');
 });
 
+router.get('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const detail = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+
+    res.render('links/edit', {detail: detail[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const { title, description, link } = req.body;
+    const updateLink = { title, description, link };
+
+    await pool.query('UPDATE links SET ? WHERE id = ?', [updateLink, id]);
+
+    res.redirect('/links');
+});
+
 module.exports = router;
